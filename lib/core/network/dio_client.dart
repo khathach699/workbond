7de123cb825e1dab7bf4c91278c173131d680620
KhaThach <khathach699@gmail.com>
2025-05-19 11:17:29 +0700

    feat: set up diretory structure

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,7 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class DioClient {
+  final Dio _dio;
+  final FlutterSecureStorage _storage;
+}
